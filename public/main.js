68a3bc66260078053d1a5a48f8ac5b01f7f2c718
Bianca Togonon <biancatogodev@gmail.com>
2022-06-28 21:29:55 -0700

    Refactored code for bubble and added comments

--- public/main.js
+++ public/main.js
@@ -24,28 +24,51 @@ const dayBox = document.querySelectorAll('.dayBox')
 const popUpContainer = document.querySelector('.pop-up-container')
 const close = document.querySelector('#close')
 
+/* BUBBLE */
+
+// Bubble: mouse over to show bubble
+// When you mouse over each box,
 dayBox.forEach(box => box.addEventListener('mouseover', () => {
+    // define the date from the box's id ('date_' + YYYY-MM-DD)
     let idDate = box.id.slice(5)
+    // define the bubble and pointer elements
     let dayBoxBubble = document.querySelectorAll(`.bubble_${idDate}`)
-    dayBoxBubble.forEach(bubble => bubble.classList.toggle('hidden'))
+    // add class 'show' to both bubble and pointer elements
+    dayBoxBubble.forEach(bubble => bubble.classList.add('show'))
 }))
 
+// Bubble: mouse out to hide bubble
+// When you mouse out of each box,
 dayBox.forEach(box => box.addEventListener('mouseout', () => {
+    // define the date from the box's id ('date_' + YYYY-MM-DD)
     let idDate = box.id.slice(5)
+    // define the bubble and pointer elements
     let dayBoxBubble = document.querySelectorAll(`.bubble_${idDate}`)
-    dayBoxBubble.forEach(bubble => bubble.classList.toggle('hidden'))
+    // remove class 'show' to hide both bubble and pointer elements
+    dayBoxBubble.forEach(bubble => bubble.classList.remove('show'))
 }))
 
+/* POP UP */
+
+// Click box to reveal pop up box
+// When you click on any of the boxes,
 dayBox.forEach(box => box.addEventListener('click', () => {
+    // define the date from the box's id ('date_' + YYYY-MM-DD)
     let idBoxClicked = box.id.slice(5)
+    // set the value of date picker to the date clicked
     setDate.setAttribute('value', idBoxClicked);
+    // add class 'show' pop up box
     popUpContainer.classList.add('show');
 }))
 
+// When you click the X in the pop up box,
 close.addEventListener('click', () => {
+    // remove class 'show' to hide pop up box
     popUpContainer.classList.remove('show')
 })
 
+/* DAY BOX COLORS */
+// Add a class to day box to change colors
 function setBoxColorClass() {
     fetch('/motd')
     .then(res => res.json())
@@ -64,7 +87,9 @@ function setBoxColorClass() {
 
 setBoxColorClass()
 
+/* DELETING ENTRIES */
 const deleteEntry = document.querySelector('#delete')
+
 deleteEntry.addEventListener('click', _ => {
     
     fetch('/motd', {
@@ -83,5 +108,3 @@ deleteEntry.addEventListener('click', _ => {
         })
         .catch(error => console.error(error))
   })
-
-  console.log(setDate.value)
